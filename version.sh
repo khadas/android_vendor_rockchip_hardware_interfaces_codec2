1fcf89200462dd54c5a68b61923a54c1d9c49962
Chen Jinsen <kevin.chen@rock-chips.com>
2023-08-10 17:40:20 +0800

    [version]: filter out unsupported symbols from version-info
    Signed-off-by: Chen Jinsen <kevin.chen@rock-chips.com>
    Change-Id: I976f71330595fa57dafa8a53b92ed2d6208a6d89

--- version.sh
+++ version.sh
@@ -2,7 +2,7 @@
 CURRENTDIR=`dirname $0`
 cd $CURRENTDIR
 
-VERSION=$(git log -1 --date=short --pretty=format:"git-%h-%s")
+VERSION=$(git log -1 --date=short --pretty=format:"git-%h-%s"|sed "s/\"//g")
 CURRENT_TIME=`date +"%Y-%m-%d %H:%M:%S"`
 PRODUCT=$TARGET_PRODUCT
 VERSION_TARGET="$(cat version.h.template | sed  -e 's/\$GIT_BUILD_VERSION/'"$VERSION build-time: $CURRENT_TIME running on $PRODUCT"'/g' version.h.template)"
